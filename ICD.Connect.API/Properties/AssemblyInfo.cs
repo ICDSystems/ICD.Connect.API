0fc88833b9fc86f53f6f24c8c89cab85879186ba
Chris Cameron <chris.cameron@wedoresi.com>
2018-01-11 13:04:43 -0500

    Adding AssemblyCompany

--- ICD.Connect.API/Properties/AssemblyInfo.cs
+++ ICD.Connect.API/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("ICD.Connect.API")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.API")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
 [assembly: AssemblyVersion("1.0.0.*")]
