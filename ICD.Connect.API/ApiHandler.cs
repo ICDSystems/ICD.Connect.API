ba9cf027ce83fbafe8adca7d32240189aa41854c
Chris Cameron <chris.cameron@wedoresi.com>
2018-02-14 09:28:29 -0500

    Revert "Begin generating output feedback for a given API request"
    This reverts commit a989f7d86a127aaf46467f4d62b11c49a819cc09.

--- ICD.Connect.API/ApiHandler.cs
+++ ICD.Connect.API/ApiHandler.cs
@@ -11,7 +11,6 @@ using ICD.Common.Utils.Services.Logging;
 using ICD.Connect.API.Attributes;
 using ICD.Connect.API.Info;
 using ICD.Connect.API.Nodes;
-using ICD.Common.Utils.Extensions;
 
 namespace ICD.Connect.API
 {
@@ -29,28 +28,23 @@ namespace ICD.Connect.API
 		/// <summary>
 		/// Interprets the incoming API request.
 		/// </summary>
-		/// <param name="json"></param>
-		/// <returns></returns>
-		public static ApiClassInfo HandleRequest(string json)
+		/// <param name="serialized"></param>
+		public static void HandleRequest(string serialized)
 		{
-			ApiClassInfo info = ApiClassInfo.Deserialize(json);
-			return HandleRequest(info);
+			ApiClassInfo info = ApiClassInfo.Deserialize(serialized);
+			HandleRequest(info);
 		}
 
 		/// <summary>
 		/// Interprets the incoming API request.
 		/// </summary>
-		/// <param name="request"></param>
-		/// <returns></returns>
-		public static ApiClassInfo HandleRequest(ApiClassInfo request)
+		/// <param name="info"></param>
+		public static void HandleRequest(ApiClassInfo info)
 		{
-			if (request == null)
+			if (info == null)
 				throw new ArgumentNullException("info");
 
-			ApiClassInfo response = request.DeepCopy();
-			HandleRequest(request, response, typeof(ApiHandler), null);
-
-			return response;
+			HandleRequest(info, typeof(ApiHandler), null);
 		}
 
 		#endregion
@@ -60,38 +54,36 @@ namespace ICD.Connect.API
 		/// <summary>
 		/// Interprets the incoming API request.
 		/// </summary>
-		/// <param name="request"></param>
-		/// <param name="output"></param>
+		/// <param name="info"></param>
 		/// <param name="type"></param>
 		/// <param name="instance"></param>
-		private static void HandleRequest(ApiClassInfo request, ApiClassInfo output, Type type, object instance)
+		private static void HandleRequest(ApiClassInfo info, Type type, object instance)
 		{
-			if (request == null)
+			if (info == null)
 				throw new ArgumentNullException("info");
 
 			type = instance == null ? type : instance.GetType();
-			bool handled = false;
 
-			handled |= ZipHandleRequest(request.GetMethods(), output.GetMethods(), (a, b) => HandleRequest(a, b, type, instance));
-			handled |= ZipHandleRequest(request.GetProperties(), output.GetProperties(), (a, b) => HandleRequest(a, b, type, instance));
-			handled |= ZipHandleRequest(request.GetNodes(), output.GetNodes(), (a, b) => HandleRequest(a, b, type, instance));
-			handled |= ZipHandleRequest(request.GetNodeGroups(), output.GetNodeGroups(), (a, b) => HandleRequest(a, b, type, instance));
+			foreach (ApiMethodInfo method in info.GetMethods())
+				HandleRequest(method, type, instance);
 
-			if (handled)
-				return;
+			foreach (ApiPropertyInfo property in info.GetProperties())
+				HandleRequest(property, type, instance);
 
-			// If the command wasn't handled at this stage we respond with info about the features on this class
-			throw new NotImplementedException();
+			foreach (ApiNodeInfo node in info.GetNodes())
+				HandleRequest(node, type, instance);
+
+			foreach (ApiNodeGroupInfo nodeGroup in info.GetNodeGroups())
+				HandleRequest(nodeGroup, type, instance);
 		}
 
 		/// <summary>
 		/// Interprets the incoming API request.
 		/// </summary>
 		/// <param name="node"></param>
-		/// <param name="output"></param>
 		/// <param name="type"></param>
 		/// <param name="instance"></param>
-		private static void HandleRequest(ApiNodeInfo node, ApiNodeInfo output, Type type, object instance)
+		private static void HandleRequest(ApiNodeInfo node, Type type, object instance)
 		{
 			type = instance == null ? type : instance.GetType();
 
@@ -106,40 +98,39 @@ namespace ICD.Connect.API
 				                property.PropertyType
 				                : nodeValue.GetType();
 
-			HandleRequest(node.Node, output.Node, nodeType, nodeValue);
+			HandleRequest(node.Node, nodeType, nodeValue);
 		}
 
 		/// <summary>
 		/// Interprets the incoming API request.
 		/// </summary>
 		/// <param name="nodeGroup"></param>
-		/// <param name="output"></param>
 		/// <param name="type"></param>
 		/// <param name="instance"></param>
-		private static void HandleRequest(ApiNodeGroupInfo nodeGroup, ApiNodeGroupInfo output, Type type, object instance)
+		private static void HandleRequest(ApiNodeGroupInfo nodeGroup, Type type, object instance)
 		{
 			type = instance == null ? type : instance.GetType();
 
 			PropertyInfo property = ApiNodeGroupAttribute.GetProperty(nodeGroup, type);
 			IApiNodeGroup group = property.GetValue(instance, new object[0]) as IApiNodeGroup;
 
-			ZipHandleRequest(nodeGroup, output, (a, b) =>
-			                                    {
-				                                    object classInstance = group[a.Key];
-				                                    Type classType = classInstance.GetType();
+			foreach (KeyValuePair<uint, ApiClassInfo> kvp in nodeGroup)
+			{
+				ApiClassInfo classInfo = kvp.Value;
+				object classInstance = group[kvp.Key];
+				Type classType = classInstance.GetType();
 
-				                                    HandleRequest(a.Value, b.Value, classType, classInstance);
-			                                    });
+				HandleRequest(classInfo, classType, classInstance);
+			}
 		}
 
 		/// <summary>
 		/// Interprets the incoming API request.
 		/// </summary>
 		/// <param name="info"></param>
-		/// <param name="output"></param>
 		/// <param name="type"></param>
 		/// <param name="instance"></param>
-		private static void HandleRequest(ApiMethodInfo info, ApiMethodInfo output, Type type, object instance)
+		private static void HandleRequest(ApiMethodInfo info, Type type, object instance)
 		{
 			type = instance == null ? type : instance.GetType();
 
@@ -165,10 +156,9 @@ namespace ICD.Connect.API
 		/// Interprets the incoming API request.
 		/// </summary>
 		/// <param name="info"></param>
-		/// <param name="output"></param>
 		/// <param name="type"></param>
 		/// <param name="instance"></param>
-		private static void HandleRequest(ApiPropertyInfo info, ApiPropertyInfo output, Type type, object instance)
+		private static void HandleRequest(ApiPropertyInfo info, Type type, object instance)
 		{
 			type = instance == null ? type : instance.GetType();
 
@@ -198,30 +188,6 @@ namespace ICD.Connect.API
 				return Enum.ToObject(type, value);
 
 			return Convert.ChangeType(value, type, null);
-		}
-
-		/// <summary>
-		/// Returns true if there was anything in the sequence.
-		/// </summary>
-		/// <typeparam name="TFirst"></typeparam>
-		/// <typeparam name="TSecond"></typeparam>
-		/// <param name="first"></param>
-		/// <param name="second"></param>
-		/// <param name="callback"></param>
-		/// <returns></returns>
-		private static bool ZipHandleRequest<TFirst, TSecond>(this IEnumerable<TFirst> first,
-		                                                      IEnumerable<TSecond> second,
-		                                                      Action<TFirst, TSecond> callback)
-		{
-			bool output = false;
-
-			first.Zip(second, (a, b) =>
-			                  {
-				                  output = true;
-				                  callback(a, b);
-			                  });
-
-			return output;
 		}
 
 		#endregion
