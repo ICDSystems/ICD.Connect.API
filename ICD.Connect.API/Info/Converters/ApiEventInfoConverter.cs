00f2e42a706ed3d0a8b6b221f41c77dc5e19a3d0
Chris Cameron <chris.cameron@wedoresi.com>
2018-04-09 16:24:41 -0400

    feat: Adding subscription action serialization/deserialization to ApiEventInfoConverter

--- ICD.Connect.API/Info/Converters/ApiEventInfoConverter.cs
+++ ICD.Connect.API/Info/Converters/ApiEventInfoConverter.cs
@@ -1,9 +1,12 @@
-﻿using Newtonsoft.Json;
+﻿using ICD.Common.Utils.Extensions;
+using Newtonsoft.Json;
 
 namespace ICD.Connect.API.Info.Converters
 {
-    public sealed class ApiEventInfoConverter : AbstractApiInfoConverter<ApiEventInfo>
-    {
+	public sealed class ApiEventInfoConverter : AbstractApiInfoConverter<ApiEventInfo>
+	{
+		private const string PROPERTY_ACTION = "action";
+
 		/// <summary>
 		/// Creates a new instance of T.
 		/// </summary>
@@ -14,14 +17,38 @@ namespace ICD.Connect.API.Info.Converters
 		}
 
 		/// <summary>
+		/// Override to serialize additional properties to the JSON.
+		/// </summary>
+		/// <param name="writer"></param>
+		/// <param name="value"></param>
+		/// <param name="serializer"></param>
+		protected override void WriteProperties(JsonWriter writer, ApiEventInfo value, JsonSerializer serializer)
+		{
+			base.WriteProperties(writer, value, serializer);
+
+			if (value.SubscribeAction != ApiEventInfo.eSubscribeAction.None)
+			{
+				writer.WritePropertyName(PROPERTY_ACTION);
+				writer.WriteValue(value.SubscribeAction);
+			}
+		}
+
+		/// <summary>
 		/// Override to handle the current property value with the given name.
 		/// </summary>
 		/// <param name="property"></param>
 		/// <param name="reader"></param>
 		/// <param name="instance"></param>
 		/// <param name="serializer"></param>
-		protected override void ReadProperty(string property, JsonReader reader, ApiEventInfo instance, JsonSerializer serializer)
+		protected override void ReadProperty(string property, JsonReader reader, ApiEventInfo instance,
+		                                     JsonSerializer serializer)
 		{
+			switch (property)
+			{
+				case PROPERTY_ACTION:
+					instance.SubscribeAction = reader.GetValueAsEnum<ApiEventInfo.eSubscribeAction>();
+					break;
+			}
 		}
 	}
 }
