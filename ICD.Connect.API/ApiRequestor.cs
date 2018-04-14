017a2cfdf31cbcd874c2639f9b0ed973c57461d3
Chris Cameron <chris.cameron@wedoresi.com>
2018-04-14 18:08:50 -0400

    feat: Simple IApiRequestor implementation for getting API feedback as an event

---
+++ ICD.Connect.API/ApiRequestor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ICD.Common.Utils.Extensions;
+using ICD.Connect.API;
+using ICD.Connect.API.Info;
+
+namespace ICD.Connect.Krang.Remote.Direct.API
+{
+    public sealed class ApiRequestor : IApiRequestor
+	{
+		/// <summary>
+		/// Raised when the API sends event feedback commands.
+		/// </summary>
+		public event EventHandler<ApiClassInfoEventArgs> OnApiFeedback;
+
+		/// <summary>
+		/// Handles an event feedback command from the ApiHandler.
+		/// </summary>
+		/// <param name="command"></param>
+		public void HandleFeedback(ApiClassInfo command)
+		{
+			OnApiFeedback.Raise(this, new ApiClassInfoEventArgs(command));
+		}
+	}
+}
---
+++ ICD.Connect.API/EventArguments/ApiClassInfoEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using ICD.Common.Utils.EventArguments;
+using ICD.Connect.API.Info;
+
+namespace ICD.Connect.API.EventArguments
+{
+    public sealed class ApiClassInfoEventArgs : GenericEventArgs<ApiClassInfo>
+    {
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="data"></param>
+		public ApiClassInfoEventArgs(ApiClassInfo data)
+			: base(data)
+		{
+		}
+	}
+}
