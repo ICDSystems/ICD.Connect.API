be4dde8bc6ec253d9acf008de1542ecc91fb37e8
Chris Cameron <chris.cameron@wedoresi.com>
2019-02-11 12:19:08 -0500

    refactor: Tidying

--- ICD.Connect.API/Info/ApiMethodInfo.cs
+++ ICD.Connect.API/Info/ApiMethodInfo.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using ICD.Common.Properties;
-using ICD.Common.Utils.Extensions;
 using ICD.Connect.API.Attributes;
 using ICD.Connect.API.Info.Converters;
 using Newtonsoft.Json;
