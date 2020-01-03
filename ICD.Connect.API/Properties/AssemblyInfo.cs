9e515517edcfce4170b3d4bf2619276f88e3576e
Chris Cameron <chris.cameron@wedoresi.com>
2020-01-03 10:28:27 -0500

    chore: Updating AssemblyInfo

--- ICD.Connect.API/Properties/AssemblyInfo.cs
+++ ICD.Connect.API/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.API")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.API")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
 [assembly: AssemblyVersion("4.1.1.0")]
