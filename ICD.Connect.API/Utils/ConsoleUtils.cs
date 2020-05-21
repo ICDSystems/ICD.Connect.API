d4aa1f82784e79101ef1c01f10157a3def20e3e5
Chris Cameron <chris.cameron@icdsystems.com>
2020-05-20 23:17:08 -0400

    perf: Substantial console abbreviation performance improvement

--- ICD.Connect.API/Utils/ConsoleUtils.cs
+++ ICD.Connect.API/Utils/ConsoleUtils.cs
@@ -31,75 +31,72 @@ namespace ICD.Connect.API.Utils
 			if (items == null)
 				throw new ArgumentNullException("items");
 
+			// List of items as they came in
 			IList<string> itemsList = items as IList<string> ?? items.ToArray();
-			return itemsList.Select(item => FormatMinimalConsoleCommand(item, itemsList, reverse));
+
+			// List of the items sorted forwards/backwards
+			IComparer<string> comparer =
+				reverse
+					? (IComparer<string>)ReverseOrdinalIgnoreCaseComparer.Instance
+					: StringComparer.OrdinalIgnoreCase;
+			string[] sorted = itemsList.OrderBy(i => i, comparer).ToArray();
+
+			// Compare each item with adjacent items.
+			foreach (string item in itemsList)
+			{
+				int index = sorted.BinarySearch(item, comparer);
+
+				string previous = index == 0 ? null : sorted[index - 1];
+				string next = index == itemsList.Count - 1 ? null : sorted[index + 1];
+
+				int maxDeviation = 0;
+
+				if (previous != null)
+				{
+					int deviation = GetDeviationIndex(item, previous, reverse);
+					maxDeviation = Math.Max(deviation, maxDeviation);
+				}
+
+				if (next != null)
+				{
+					int deviation = GetDeviationIndex(item, next, reverse);
+					maxDeviation = Math.Max(deviation, maxDeviation);
+				}
+
+				yield return FormatMinimalConsoleCommand(item, maxDeviation, reverse);
+			}
 		}
 
 		/// <summary>
-		/// Given a console command and a sequence of console commands, formats the item with braces to show the shorthand command.
-		/// E.g.
-		/// 	ALongCommand
-		/// 	AnotherLongCommand
-		/// 	ALousyCommand
-		/// 	BCommand
-		/// Would return:
-		/// 	(ALon)gCommand
-		/// 	(An)otherLongCommand
-		/// 	(ALou)syCommand
-		/// 	(B)Command
+		/// Given a console command and an index, formats the item to highlight the shorthand command.
 		/// </summary>
 		/// <param name="item"></param>
-		/// <param name="items"></param>
+		/// <param name="index"></param>
 		/// <param name="reverse"></param>
 		/// <returns></returns>
-		private static string FormatMinimalConsoleCommand(string item, [NotNull] IEnumerable<string> items,
-		                                                  bool reverse)
+		[NotNull]
+		private static string FormatMinimalConsoleCommand([NotNull] string item, int index, bool reverse)
 		{
-			if (items == null)
-				throw new ArgumentNullException("items");
+			if (item == null)
+				throw new ArgumentNullException("item");
 
-			// Avoid comparing a console command with itself
-			string[] itemsArray = items.Where(i => !string.Equals(item, i, StringComparison.OrdinalIgnoreCase)).ToArray();
-
-			int uniqueLength = GetUniqueLength(item, itemsArray, reverse);
+			int length = index + 1;
 
 			// Easy case - there is no minimal console command
-			if (uniqueLength >= item.Length)
+			if (length >= item.Length)
 				return string.Format("{0}{1}{2}", AnsiUtils.COLOR_BLUE, item, AnsiUtils.ANSI_RESET);
 
 			return reverse
 				? string.Format("{0}{1}{2}{3}",
-				                item.Substring(0, item.Length - uniqueLength),
-								AnsiUtils.COLOR_BLUE,
-				                item.Substring(item.Length - uniqueLength),
+				                item.Substring(0, item.Length - length),
+				                AnsiUtils.COLOR_BLUE,
+				                item.Substring(item.Length - length),
 				                AnsiUtils.ANSI_RESET)
 				: string.Format("{0}{1}{2}{3}",
 				                AnsiUtils.COLOR_BLUE,
-								item.Substring(0, uniqueLength),
+				                item.Substring(0, length),
 				                AnsiUtils.ANSI_RESET,
-								item.Substring(uniqueLength));
-		}
-
-		/// <summary>
-		/// Gets the minimum number of characters of the given item to be unique against the
-		/// same number of characters from any item in the given sequence.
-		/// E.g.
-		///  	FooBar
-		///  	FooBaz
-		/// Returns
-		/// 	6
-		/// </summary>
-		/// <param name="item"></param>
-		/// <param name="items"></param>
-		/// <param name="reverse"></param>
-		/// <returns></returns>
-		private static int GetUniqueLength(string item, [NotNull] IEnumerable<string> items, bool reverse)
-		{
-			if (items == null)
-				throw new ArgumentNullException("items");
-
-			return items.Select(other => GetDeviationIndex(item, other, reverse))
-			            .MaxOrDefault() + 1;
+				                item.Substring(length));
 		}
 
 		/// <summary>
@@ -122,31 +119,41 @@ namespace ICD.Connect.API.Utils
 			if (other == null)
 				throw new ArgumentNullException("other");
 
+			item = item.ToUpper();
+			other = other.ToUpper();
+
 			int output = 0;
 
-			for (int index = 1; index <= item.Length && index <= other.Length; index++)
+			for (int index = 0; index < item.Length && index < other.Length; index++)
 			{
-				string a;
-				string b;
+				char a = reverse ? item[item.Length - (index + 1)] : item[index];
+				char b = reverse ? other[other.Length - (index + 1)] : other[index];
 
-				if (reverse)
-				{
-					a = item.Substring(item.Length - index);
-					b = other.Substring(other.Length - index);
-				}
-				else
-				{
-					a = item.Substring(0, index);
-					b = other.Substring(0, index);
-				}
-
-				if (string.Equals(a, b, StringComparison.OrdinalIgnoreCase))
-					output = index;
+				if (a == b)
+					output = index + 1;
 				else
 					break;
 			}
 
 			return output;
+		}
+
+		public sealed class ReverseOrdinalIgnoreCaseComparer : IComparer<string>
+		{
+			private static ReverseOrdinalIgnoreCaseComparer s_Instance;
+
+			public static ReverseOrdinalIgnoreCaseComparer Instance
+			{
+				get { return s_Instance ?? (s_Instance = new ReverseOrdinalIgnoreCaseComparer()); }
+			}
+
+			public int Compare(string x, string y)
+			{
+				x = x == null ? null : StringUtils.Reverse(x);
+				y = y == null ? null : StringUtils.Reverse(y);
+
+				return StringComparer.OrdinalIgnoreCase.Compare(x, y);
+			}
 		}
 	}
 }
